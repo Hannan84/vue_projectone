274210ea33aeabd9b23a465acdb99db02c5c98d0
hannan84 <hannansarkar84@gmail.com>
2025-01-13 12:05:59 +0600

    binding-data image from object

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
-const image =
-  "//images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80";
-const title = "Water surrounded by trees and mountains";
-const link = "https://unsplash.com/photos/NRQV-hBF10M";
+const image = {
+  src: "//images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+  title: "Water surrounded by trees and mountains",
+  alt: "Water surrounded by trees and mountains",
+  link: "https://unsplash.com/photos/NRQV-hBF10M",
+};
 </script>
 
 <template>
@@ -10,13 +12,13 @@ const link = "https://unsplash.com/photos/NRQV-hBF10M";
     <h1 class="text-4xl mb-10">Data Project</h1>
     <section class="main flex space-x-4">
       <div class="flex space-y-4 flex-col w-1/2">
-        <img :title="title" :src="image" :alt="title" />
+        <img v-bind="image" />
         <h2 class="text-xl text-left">
-          {{ title }}
+          {{ image.title }}
         </h2>
         <a
           target="_blank"
-          :href="link"
+          :href="image.link"
           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-40 text-center"
         >
           Image Source

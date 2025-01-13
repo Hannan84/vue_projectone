74a22e345aa09f06766b83b052e2cb708519718e
hannan84 <hannansarkar84@gmail.com>
2025-01-13 18:58:33 +0600

    v-show use

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 const location = "Dhaka";
-const weather = "s";
+const weather = "t";
 </script>
 
 <template>
@@ -8,34 +8,36 @@ const weather = "s";
     <h1 class="text-4xl mb-10">Data Project</h1>
     <div class="flex space-y-4 p-10 bg-gray-200 flex-col w-1/3 items-center">
       <h1 class="text-3xl">{{ location }}</h1>
-      <h2 class="text-2xl" v-if="weather == 's'">Weather: Sunny</h2>
-      <h2 class="text-2xl" v-else-if="weather == 'pc'">
-        Weather: Partly Cloudy
-      </h2>
-      <h2 class="text-2xl" v-else-if="weather == 'r'">Weather: Rainy</h2>
-      <h2 class="text-2xl" v-else-if="weather == 't'">Weather: Thunderstorm</h2>
-      <h2 class="text-2xl" v-else>Weather: Cyclone</h2>
+      <h2 class="text-2xl" v-show="weather == 's'">Weather: Sunny</h2>
+      <h2 class="text-2xl" v-show="weather == 'pc'">Weather: Partly Cloudy</h2>
+      <h2 class="text-2xl" v-show="weather == 'r'">Weather: Rainy</h2>
+      <h2 class="text-2xl" v-show="weather == 't'">Weather: Thunderstorm</h2>
+      <h2 class="text-2xl" v-show="weather == 'c'">Weather: Cyclone</h2>
       <img
-        v-if="weather == 's'"
+        v-show="weather == 's'"
         src="./data/icon/weather-sunny-icon.png"
         alt=""
       />
       <img
-        v-else-if="weather == 'pc'"
+        v-show="weather == 'pc'"
         src="./data/icon/weather-cloud-icon.png"
         alt=""
       />
       <img
-        v-else-if="weather == 'r'"
+        v-show="weather == 'r'"
         src="./data/icon/weather-rain-icon.png"
         alt=""
       />
       <img
-        v-else-if="weather == 't'"
+        v-show="weather == 't'"
         src="./data/icon/weather-storm-icon.png"
         alt=""
       />
-      <img v-else src="./data/icon/weather-cyclone-icon.png" alt="" />
+      <img
+        v-show="weather == 'c'"
+        src="./data/icon/weather-cyclone-icon.png"
+        alt=""
+      />
     </div>
   </section>
 </template>

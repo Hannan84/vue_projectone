db04c31fcadcaa4184c8721e6c204b4d683efe8a
hannan84 <hannansarkar84@gmail.com>
2025-01-13 18:51:56 +0600

    if-else logic

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,42 @@
 <script setup>
-const number = 16;
-const animal = "Tiger";
-const searchUrl = "https://www.pexels.com/search/";
+const location = "Dhaka";
+const weather = "s";
 </script>
 
 <template>
-  <section class="container mx-auto p-10">
+  <section class="container mx-auto flex flex-col p-10 items-center">
     <h1 class="text-4xl mb-10">Data Project</h1>
-    <h2 class="text-2xl mb-10">{{ number }} / 4 = {{ number / 4 }}</h2>
-    <h2 class="text-2xl mb-10">
-      {{ number }} is {{ number % 2 == 0 ? "even" : "odd" }}
-    </h2>
-    <h2 class="text-2xl mb-10">
-      <a :href="searchUrl + animal.toLowerCase()" target="_blank"
-        >Search for {{ animal }}</a
-      >
-    </h2>
+    <div class="flex space-y-4 p-10 bg-gray-200 flex-col w-1/3 items-center">
+      <h1 class="text-3xl">{{ location }}</h1>
+      <h2 class="text-2xl" v-if="weather == 's'">Weather: Sunny</h2>
+      <h2 class="text-2xl" v-else-if="weather == 'pc'">
+        Weather: Partly Cloudy
+      </h2>
+      <h2 class="text-2xl" v-else-if="weather == 'r'">Weather: Rainy</h2>
+      <h2 class="text-2xl" v-else-if="weather == 't'">Weather: Thunderstorm</h2>
+      <h2 class="text-2xl" v-else>Weather: Cyclone</h2>
+      <img
+        v-if="weather == 's'"
+        src="./data/icon/weather-sunny-icon.png"
+        alt=""
+      />
+      <img
+        v-else-if="weather == 'pc'"
+        src="./data/icon/weather-cloud-icon.png"
+        alt=""
+      />
+      <img
+        v-else-if="weather == 'r'"
+        src="./data/icon/weather-rain-icon.png"
+        alt=""
+      />
+      <img
+        v-else-if="weather == 't'"
+        src="./data/icon/weather-storm-icon.png"
+        alt=""
+      />
+      <img v-else src="./data/icon/weather-cyclone-icon.png" alt="" />
+    </div>
   </section>
 </template>
 

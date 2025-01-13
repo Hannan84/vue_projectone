796878c7dc315605e5006962290fc6a0a630f8de
hannan84 <hannansarkar84@gmail.com>
2025-01-13 20:10:29 +0600

    v-if items grouping and difference v-if and v-show

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,35 @@
 <script setup>
+import { ref } from "vue";
 const location = "Dhaka";
-const weather = "t";
+const weather = ref("s");
 </script>
 
 <template>
-  <section class="container mx-auto flex flex-col p-10 items-center">
+  <section class="container mx-auto flex flex-col items-center">
     <h1 class="text-4xl mb-10">Data Project</h1>
-    <div class="flex space-y-4 p-10 bg-gray-200 flex-col w-1/3 items-center">
+    <div class="w-1/3 bg-gray-200 flex space-y-10 p-10 flex-col items-center">
       <h1 class="text-3xl">{{ location }}</h1>
-      <h2 class="text-2xl" v-show="weather == 's'">Weather: Sunny</h2>
-      <h2 class="text-2xl" v-show="weather == 'pc'">Weather: Partly Cloudy</h2>
-      <h2 class="text-2xl" v-show="weather == 'r'">Weather: Rainy</h2>
-      <h2 class="text-2xl" v-show="weather == 't'">Weather: Thunderstorm</h2>
-      <h2 class="text-2xl" v-show="weather == 'c'">Weather: Cyclone</h2>
-      <img
-        v-show="weather == 's'"
-        src="./data/icon/weather-sunny-icon.png"
-        alt=""
-      />
-      <img
-        v-show="weather == 'pc'"
-        src="./data/icon/weather-cloud-icon.png"
-        alt=""
-      />
-      <img
-        v-show="weather == 'r'"
-        src="./data/icon/weather-rain-icon.png"
-        alt=""
-      />
-      <img
-        v-show="weather == 't'"
-        src="./data/icon/weather-storm-icon.png"
-        alt=""
-      />
-      <img
-        v-show="weather == 'c'"
-        src="./data/icon/weather-cyclone-icon.png"
-        alt=""
-      />
+      <template v-if="weather == 's'">
+        <h2 class="text-2xl">Weather: Sunny</h2>
+        <img id="sunny" src="./data/icon/weather-sunny-icon.png" alt="" />
+      </template>
+      <template v-if="weather == 'pc'">
+        <h2 class="text-2xl">Weather: Partly Cloudy</h2>
+        <img src="./data/icon/weather-cloud-icon.png" alt="" />
+      </template>
+      <template v-if="weather == 'r'">
+        <h2 class="text-2xl">Weather: Rainy</h2>
+        <img src="./data/icon/weather-rain-icon.png" alt="" />
+      </template>
+      <template v-if="weather == 't'">
+        <h2 class="text-2xl">Weather: Thunderstorm</h2>
+        <img src="./data/icon/weather-storm-icon.png" alt="" />
+      </template>
+      <template v-if="weather == 'c'">
+        <h2 class="text-2xl">Weather: Cyclone</h2>
+        <img src="./data/icon/weather-cyclone-icon.png" alt="" />
+      </template>
+      <input type="text" name="" id="" class="text-center" v-model="weather" />
     </div>
   </section>
 </template>

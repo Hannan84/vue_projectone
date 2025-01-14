a7df2d0200f73bbf4e01e13ca6d139a7eda51498
hannan84 <hannansarkar84@gmail.com>
2025-01-14 16:56:20 +0600

    class binding

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,13 @@
-<script setup>
-import { ref } from "vue";
-const location = "Dhaka";
-const weather = ref("s");
-</script>
+<script setup></script>
 
 <template>
-  <section class="container mx-auto flex flex-col items-center">
-    <h1 class="text-4xl mb-10">Data Project</h1>
-    <div class="w-1/3 bg-gray-200 flex space-y-10 p-10 flex-col items-center">
-      <h1 class="text-3xl">{{ location }}</h1>
-      <template v-if="weather == 's'">
-        <h2 class="text-2xl">Weather: Sunny</h2>
-        <img id="sunny" src="./data/icon/weather-sunny-icon.png" alt="" />
-      </template>
-      <template v-if="weather == 'pc'">
-        <h2 class="text-2xl">Weather: Partly Cloudy</h2>
-        <img src="./data/icon/weather-cloud-icon.png" alt="" />
-      </template>
-      <template v-if="weather == 'r'">
-        <h2 class="text-2xl">Weather: Rainy</h2>
-        <img src="./data/icon/weather-rain-icon.png" alt="" />
-      </template>
-      <template v-if="weather == 't'">
-        <h2 class="text-2xl">Weather: Thunderstorm</h2>
-        <img src="./data/icon/weather-storm-icon.png" alt="" />
-      </template>
-      <template v-if="weather == 'c'">
-        <h2 class="text-2xl">Weather: Cyclone</h2>
-        <img src="./data/icon/weather-cyclone-icon.png" alt="" />
-      </template>
-      <input
-        type="text"
-        name=""
-        id=""
-        class="text-center py-2"
-        v-model="weather"
-      />
-    </div>
+  <section class="container mx-auto">
+    <h1 class="text-center text-3xl py-12">Class & Style Binding</h1>
+    <section class="flex space-x-10">
+      <div class="w-1/3 bg-orange-400 h-16"></div>
+      <div class="w-1/3 bg-indigo-400 h-16"></div>
+      <div class="w-1/3 bg-green-400 h-16"></div>
+    </section>
   </section>
 </template>
 

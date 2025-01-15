b10c090646a5cf7cb5c7a371662cad6dad09fbc1
hannan84 <hannansarkar84@gmail.com>
2025-01-15 15:58:00 +0600

    display index in loo[

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
-const items = [1, 2, 3, 4, 5];
+const tasks = [
+  "Project Planning and Strategy",
+  "Team Collaboration and Coordination",
+  "Market Research and Analysis",
+  "Customer Feedback and Improvement",
+  "Data Collection and Reporting",
+];
 </script>
 
 <template>
   <section class="container mx-auto flex flex-col items-center space-y-2">
-    <!-- <div class="p-5 border border-gray-600 w-40" v-for="item in items">
-      {{ item }}
-    </div> -->
-    <div class="p-5 border border-gray-600 w-40" v-for="n in 5">
-      {{ n }}
+    <div class="p-5 border border-gray-600 w-50" v-for="(task, index) in tasks">
+      {{ index + 1 }}. {{ task }}
     </div>
   </section>
 </template>
